d9681435a2217a3c97640ceb78283743b81244dc
Farhang Vafa <farhangv@gmail.com>
2025-01-25 13:09:38 -0500

    Red enemy attacks

---
+++ Assets/Scripts/NPC/EnemyAttack.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float speed = 6f;
+    Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriteRenderer;
+    private Transform player;
+    private bool hasTouchedIntestineWalls = false;
+
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        if (playerObject != null)
+        {
+            player = playerObject.transform;
+        }
+    }
+
+    void Update()
+    {
+        if (player != null && hasTouchedIntestineWalls)
+        {
+            Vector3 direction = (player.position - transform.position).normalized;
+            rigidbody2D.MovePosition(transform.position + direction * speed * Time.deltaTime);
+
+            speed += Time.deltaTime * 0.1f;
+        }
+        if (player.position.x < transform.position.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("IntestineWall"))
+        {
+            hasTouchedIntestineWalls = true;
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Enemy attacked the player!");
+
+            // PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+            // if (playerHealth != null)
+            // {
+            //     playerHealth.TakeDamage(10); // Example damage value
+            // }
+
+            // Destroy(gameObject);
+        }
+    }
+}
