1c76959b892891aa347aafe2c9aacf3538c0c8d6
ValtchanV <ValtchanV@GMail.com>
2025-01-26 11:26:27 -0500

    added pizza spawner

--- Assets/Scripts/Items/Spawner.cs
+++ Assets/Scripts/Items/Spawner.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Collections;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class Spawner : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class Spawner : MonoBehaviour
 
     [SerializeField] float _delay = 2f;
     [SerializeField] float _interval = 10f;
-
+    [SerializeField] bool _isSingleton = true;
 
     void Awake()
     {
@@ -21,7 +22,13 @@ public class Spawner : MonoBehaviour
         while (true)
         {
             var o = _catalogue?.ElementAtOrDefault(0);
-            if (o != null) Instantiate(_catalogue[0], transform.position, transform.rotation);
+            if (o != null)
+            {
+                Debug.Log("Spawning " + o.name);
+                var lastSpawn = Instantiate(_catalogue[0], transform.position, transform.rotation);
+                while (_isSingleton && !lastSpawn.IsDestroyed()) yield return new WaitForEndOfFrame();
+            }
+            
             yield return new WaitForSeconds(_interval);
         }
     }
