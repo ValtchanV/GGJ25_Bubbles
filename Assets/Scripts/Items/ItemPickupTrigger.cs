f375104f0a85eb7ecffee4195a01a45d8161f281
ValtchanV <ValtchanV@GMail.com>
2025-01-26 02:57:28 -0500

    fixed pickup bug

--- Assets/Scripts/Items/ItemPickupTrigger.cs
+++ Assets/Scripts/Items/ItemPickupTrigger.cs
@@ -4,6 +4,7 @@ public class ItemPickupTrigger : MonoBehaviour
     [SerializeField] string ItemName = "Corn";
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if(other.name != "X") return;
         GameManager.GetGameManager().OnItemPickup(ItemName);
         Destroy(gameObject.transform.parent.gameObject);
     }
